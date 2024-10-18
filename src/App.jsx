44fd88b40438217d2c78fc7adab3d701eb067b9c
Aniketgarate11 <garateaniket11@gmail.com>
2024-10-18 15:49:29 +0530

    minor modifi.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import './App.css'
 function App() {
 
 
-  console.log(import.meta.env.VITE_APP_NAME);
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
 
   return (
     <>
