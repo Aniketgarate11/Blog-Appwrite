8d31ba02f51279cb5ba144d671f03137ffefb249
Aniketgarate11 <garateaniket11@gmail.com>
2024-10-24 20:22:24 +0530

    add  post and file services

--- src/appwrite/configure.js
+++ src/appwrite/configure.js
@@ -10,8 +10,63 @@ export class Service {
     this.client
       .setEndpoint(conf.appwriteUrl)
       .setProject(conf.appwriteProjectId);
+
+      this.databases = new Databases(this.client)
+      this.bucket = new Storage(this.client)
   }
 
+  async createPost({title, slug, content, image, status, userId}){
+    try {
+        return await this.databases.createDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                image,
+                status,
+                userId
+            }
+        )
+        
+    } catch (error) {
+        console.log(" Appwrite Service :: createPost :: error", error)
+    }
+  }
+
+  async updatePost(slug,{title, content, image, status}){
+    try {
+        return await this.databases.updateDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                image,
+                status
+            }
+        )
+        
+    } catch (error) {
+        console.log(" Appwrite Service :: updatePost :: error", error)
+    }
+  }
+
+  async deletePost(slug){
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      )
+        
+    } catch (error) {
+        console.log(" Appwrite Service :: deletePost :: error", error);
+        return false;
+    }
+  }
 }
 
 const service = new Service();
